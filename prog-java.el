b69d9668d850abb997a47add75161b346960b0f7
Ben Spencer <dangerous.ben@gmail.com>
2019-10-08 12:01:43 +0100

    Java indent level.

--- prog-java.el
+++ prog-java.el
@@ -1,6 +1,11 @@
+(defun my-java-mode-hook()
+  (setq c-basic-offset 2))
+
 (use-package java-mode
   :config (add-to-list 'lsp-file-watch-ignored "\\.class$")
-  :hook (java-mode . lsp))
+  :hook
+  (java-mode . my-java-mode-hook)
+  (java-mode . lsp))
 
 (use-package lsp-java
   :ensure t)
