0cdaaa3cdfdbc5b808f71b095fcb240b12cab0e7
Ben Spencer <dangerous.ben@gmail.com>
2019-10-18 13:38:11 +0100

    Needs to happen in init?

--- prog-java.el
+++ prog-java.el
@@ -2,7 +2,7 @@
   (setq c-basic-offset 2))
 
 (use-package java-mode
-  :config (add-to-list 'lsp-file-watch-ignored "\\.class$")
+  :init (add-to-list 'lsp-file-watch-ignored "\\.class$")
   :hook
   (java-mode . my-java-mode-hook)
   (java-mode . lsp))
--- prog-scala.el
+++ prog-scala.el
@@ -1,7 +1,7 @@
 (use-package scala-mode
   :ensure t
   :mode "\\.s\\(cala\\|bt\\)$"
-  :config (add-to-list 'lsp-file-watch-ignored "\\.class$")
+  :init (add-to-list 'lsp-file-watch-ignored "\\.class$")
   :bind (:map scala-mode-map ("C-c C-v s" . sbt-start))
   :hook (scala-mode . lsp))
 
