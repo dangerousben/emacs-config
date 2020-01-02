99b5f7dfaf858f06757ebed1b3a3fd7ce118d46e
Ben Spencer <dangerous.ben@gmail.com>
2020-01-02 06:59:45 +0000

    Disable web mode automatic quoting.

--- prog-web.el
+++ prog-web.el
@@ -2,6 +2,7 @@
   :ensure t
   :custom
   (web-mode-enable-auto-indentation nil)
+  (web-mode-enable-auto-quoting nil)
   (web-mode-code-indent-offset 2)
   (web-mode-css-indent-offset 2)
   (web-mode-markup-indent-offset 2)
