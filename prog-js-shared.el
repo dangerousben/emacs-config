893819347e43c34df8aa479271689844a8c648bc
Ben Spencer <dangerous.ben@gmail.com>
2020-01-09 11:43:10 +0000

    Set prettier print width.

--- prog-js-shared.el
+++ prog-js-shared.el
@@ -4,5 +4,6 @@
 
 (use-package prettier-js
   :ensure t
+  :custom (prettier-js-args '("--print-width", "120"))
   :custom (prettier-js-show-errors nil)
   :hook ((js-mode typescript-mode web-mode) . prettier-js-mode))
