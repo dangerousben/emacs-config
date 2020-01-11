07f89bd7a6857fed3d294855d33699190673174f
Ben Spencer <dangerous.ben@gmail.com>
2020-01-11 12:38:54 +0000

    Use scala mode for ammonite scripts + no need to re-add .scala and .sbt.

--- prog-scala.el
+++ prog-scala.el
@@ -1,6 +1,6 @@
 (use-package scala-mode
   :ensure t
-  :mode "\\.s\\(cala\\|bt\\)$"
+  :mode ("\\.sc\\'")
   :init (add-to-list 'lsp-file-watch-ignored "\\.class$")
   :bind (:map scala-mode-map ("C-c C-v s" . sbt-start))
   :hook (scala-mode . lsp))
