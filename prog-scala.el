eb18d54c30bcbb95161566978ab93b25ce6e35b1
Ben Spencer <dangerous.ben@gmail.com>
2020-02-12 07:53:28 +0000

    Prettify and variable pitch for scala-mode.

--- prog-scala.el
+++ prog-scala.el
@@ -1,7 +1,14 @@
+(defun my-scala-mode-hook ()
+  (setq prettify-symbols-alist
+        (append scala-mode-pretty-arrows-alist
+                '(("<=" . ?≤) (">=" . ?≥) ("==" . ?≡) ("===" . ?≣))))
+  (variable-pitch-mode)
+  (lsp))
+
 (use-package scala-mode
   :mode ("\\.sc\\'")
   :bind (:map scala-mode-map ("C-c C-v s" . sbt-start))
-  :hook (scala-mode . lsp))
+  :hook (scala-mode . my-scala-mode-hook))
 
 (use-package sbt-mode
   :custom (sbt:program-options '("-no-colors" "-java-home" "/home/ben/opt/jdk")))
