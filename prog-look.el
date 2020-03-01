f338bef8ab1c22627711d38eb3e3b985056e68f3
Ben Spencer <dangerous.ben@gmail.com>
2020-03-01 18:36:18 +0000

    Try out Noto Sans as a coding font.

--- prog-look.el
+++ prog-look.el
@@ -1,7 +1,10 @@
 ;; Consistent look for all code, daeliberately not called "face" or anything else that is used elsewhere because I'm not entirely sure what it will cover yet
 
+(defface code-base
+  '((t :family "Noto Sans"))
+  "Basic face for code")
 (defun set-prog-look ()
-  (variable-pitch-mode)
-  (text-scale-set 1))
+  (buffer-face-set '(code-base))
+  (text-scale-set 2))
 
 (add-hook 'prog-mode-hook 'set-prog-look)
