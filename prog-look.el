d424206b675671fd27f2da1929fb2b0943bdbc6a
Ben Spencer <dangerous.ben@gmail.com>
2020-03-06 15:18:26 +0000

    Or maybe source code pro, variable edition.

--- prog-look.el
+++ prog-look.el
@@ -1,7 +1,7 @@
 ;; Consistent look for all code, daeliberately not called "face" or anything else that is used elsewhere because I'm not entirely sure what it will cover yet
 
 (defface code-base
-  '((t :family "Noto Sans"))
+  '((t :family "Source Code Variable"))
   "Basic face for code")
 (defun set-prog-look ()
   (buffer-face-set '(code-base))
