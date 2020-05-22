13514f7933151e2a00ba7268ac0ebc630ed6e33c
Ben Spencer <dangerous.ben@gmail.com>
2020-05-22 17:02:18 +0100

    Java decompiler (jdecomp / procyon).

--- prog-java.el
+++ prog-java.el
@@ -8,3 +8,10 @@
   (java-mode . lsp))
 
 (use-package lsp-java)
+
+(use-package jdecomp
+  :config (jdecomp-mode 1)
+  :custom
+  (jdecomp-decompiler-type 'cfr)
+  (jdecomp-decompiler-paths '((cfr . "~/opt/cfr.jar")
+                              (procyon . "/usr/share/java/procyon-decompiler.jar"))))
