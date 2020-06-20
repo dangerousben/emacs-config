4f92d451fbe57afa5db686f22861f159c5602cd2
Ben Spencer <dangerous.ben@gmail.com>
2020-06-20 19:18:33 +0100

    Disable visual-line-mode in yaml-mode.

--- prog-yaml.el
+++ prog-yaml.el
@@ -1,1 +1,6 @@
-(use-package yaml-mode)
+(defun my-yaml-mode-hook ()
+  ; yaml-mode is derived from text-mode
+  (visual-line-mode -1))
+
+(use-package yaml-mode
+  :hook (yaml-mode . my-yaml-mode-hook))
