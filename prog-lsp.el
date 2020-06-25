1af5064b4faa1fcc66a1af114b92e7ab1a7429cc
Ben Spencer <dangerous.ben@gmail.com>
2020-06-25 18:00:38 +0100

    Tidy up lsp config, use :custom rather than setq.

--- prog-lsp.el
+++ prog-lsp.el
@@ -1,9 +1,9 @@
 (use-package lsp-mode
-  :custom (lsp-file-watch-threshold 8192)
-  :config
-  (setq lsp-enable-snippet nil
-        lsp-idle-delay 0.500
-        lsp-prefer-capf t)
+  :custom
+  (lsp-enable-snippet nil)
+  (lsp-file-watch-threshold 8192)
+  (lsp-idle-delay 0.500)
+  (lsp-prefer-capf t)
   :bind
   (:map lsp-mode-map
         ("C-c d" . lsp-describe-thing-at-point)
