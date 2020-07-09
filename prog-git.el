49cc8f0d0d612c79e1ca4fc05c4da87ce43670bc
Ben Spencer <dangerous.ben@gmail.com>
2020-07-09 23:22:10 +0100

    gited.

--- prog-git.el
+++ prog-git.el
@@ -1,2 +1,12 @@
 (use-package git-commit
   :custom git-commit-fill-column 72)
+
+(defun gited-list-local-branches ()
+  (interactive)
+  (gited-list-branches "local"))
+
+(use-package gited
+  :custom
+  (gited-verbose t)
+  :bind (:map prog-prefix-map
+              ("B" . gited-list-local-branches)))
