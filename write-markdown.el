6bd94e2a2386bd6ffb9c5c11699a7563fc9894f4
Ben Spencer <dangerous.ben@gmail.com>
2020-07-11 18:04:04 +0100

    Configure markdown-mode properly and default to gfm.

--- write-markdown.el
+++ write-markdown.el
@@ -1,3 +1,6 @@
-(add-hook 'markdown-mode-hook 'my-writing-mode-hook)
+(use-package markdown-mode
+  :custom (markdown-command "pandoc -f gfm")
+  :mode (("\\.md\\'" . gfm-mode))
+  :hook (markdown-mode . my-writing-mode-hook))
 
 (use-package poly-markdown)
