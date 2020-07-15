b0fb06dc22147e0c692e116e9bff96101a5eba88
Ben Spencer <dangerous.ben@gmail.com>
2020-07-15 22:41:49 +0100

    Metadata for markdown (hopefully the yaml frontmatter type I want).

--- write-markdown.el
+++ write-markdown.el
@@ -1,5 +1,7 @@
 (use-package markdown-mode
-  :custom (markdown-command "pandoc -f gfm")
+  :custom
+  (markdown-command "pandoc -f gfm")
+  (markdown-use-pandoc-style-yaml-metadata)
   :mode (("\\.md\\'" . gfm-mode))
   :hook (markdown-mode . my-writing-mode-hook))
 
