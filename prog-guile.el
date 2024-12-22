491cb028f3afb3c3601f6384e0d4d697cee29a53
Ben Spencer <dangerous.ben@gmail.com>
2024-12-22 16:13:50 +0000

    prog-guile: add guix and non-guix to load path

--- prog-guile.el
+++ prog-guile.el
@@ -1,1 +1,4 @@
-(use-package geiser-guile)
+(use-package geiser-guile
+  :config
+  (add-to-list 'geiser-guile-load-path "~/src/guix/guix")
+  (add-to-list 'geiser-guile-load-path "~/src/guix/nonguix"))
