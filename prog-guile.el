fb5974d670ab9a5fdf87d189fef60cbbb5f300e5
Ben Spencer <dangerous.ben@gmail.com>
2025-02-12 00:59:08 +0000

    prog-guile: add personal guix config to load path

--- prog-guile.el
+++ prog-guile.el
@@ -1,4 +1,5 @@
 (use-package geiser-guile
   :config
   (add-to-list 'geiser-guile-load-path "~/src/guix/guix")
-  (add-to-list 'geiser-guile-load-path "~/src/guix/nonguix"))
+  (add-to-list 'geiser-guile-load-path "~/src/guix/nonguix")
+  (add-to-list 'geiser-guile-load-path "~/src/personal/guix-config"))
