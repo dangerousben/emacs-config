05ca552a5fa6ff50cf558576b0ded2de1c41cedb
Ben Spencer <dangerous.ben@gmail.com>
2025-02-20 19:14:25 +0000

    add stuff to geiser-guile-load-path
    Eventually I think this should probably be based on GUILE_LOAD_PATH,
    with additions from .dir-locales.el as suggested by the docs

--- prog-guile.el
+++ prog-guile.el
@@ -1,5 +1,7 @@
 (use-package geiser-guile
   :config
+  (add-to-list 'geiser-guile-load-path "~/src/guile/guile/module")
   (add-to-list 'geiser-guile-load-path "~/src/guix/guix")
   (add-to-list 'geiser-guile-load-path "~/src/guix/nonguix")
+  (add-to-list 'geiser-guile-load-path "~/src/guile/mcron/src")
   (add-to-list 'geiser-guile-load-path "~/src/personal/guix-config"))
