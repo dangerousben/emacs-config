a379702dfd173f0a603f3958b3abcf2d8d7b1760
Ben Spencer <dangerous.ben@gmail.com>
2025-02-20 19:13:57 +0000

    argh emacs just look right plz damn you!

--- prog-look.el
+++ prog-look.el
@@ -1,9 +1,9 @@
 ;; Consistent look for all code, daeliberately not called "face" or anything else that is used elsewhere because I'm not entirely sure what it will cover yet
 
-(defface code-base
-  '((t :family "Source Code Variable"))
-  "Basic face for code")
-(defun set-prog-look ()
-  (buffer-face-set '(code-base)))
+;; (defface code-base
+;;   '((t :family "Source Code Variable"))
+;;   "Basic face for code")
+(defun set-prog-look ())
+;;  (buffer-face-set '(code-base)))
 
-(add-hook 'prog-mode-hook 'set-prog-look)
+;; (add-hook 'prog-mode-hook 'set-prog-look)
